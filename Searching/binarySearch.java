87469d5fecbdc759dc669ec44d582772f93d1c26
Aditya Banshkar <aditya007banshkar25@gmail.com>
2023-02-05 11:41:52 +0530

    binary Search

--- Searching/binarySearch.java
+++ Searching/binarySearch.java
@@ -1,5 +1,7 @@
 package Searching;
 
+import java.util.Arrays;
+
 public class binarySearch {
     public static int binaryS(int[] a, int find){
         int low=0;
@@ -28,12 +30,23 @@ public class binarySearch {
 
     public static void main(String[] args) {
         int[] a={2,4,5,7,8,12,34};
-        int find=4;
+        int find=34;
+        // 1 logic divide and conqure
+        System.out.println("1 Divide And Conqure");
         int i=binaryS(a, find);
         if(i>=0)
             System.out.println("Element found at :"+i);
         else
             System.out.println("Not Found");
-        
+
+        //2 using Arrays.binarySearch() inbit method
+
+        System.out.println("2 Using Arrays.binarySearch()");
+        int index=Arrays.binarySearch(a,find);
+        if(index>=0)
+            System.out.println("Element found at :"+index);
+        else
+            System.out.println("Not Found");
+
     }
 }
