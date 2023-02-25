5a9cee08bc4432aef6030149e7dd8b857bd9346a
Aditya Banshkar <aditya007banshkar25@gmail.com>
2023-02-25 14:41:46 +0530

    heap

---
+++ Heap/kClosestNumber.java
@@ -0,0 +1,28 @@
+package Heap;
+//find k closest no to x
+//k=3 and x=7 so i.e find 3 no which are closest to 7
+
+import java.util.PriorityQueue;
+
+public class kClosestNumber {
+    public static void main(String[] args) {
+        int[] arr={5,6,7,8,9};
+        int k=3;
+        int x=7;
+
+        //logic subtracting all no by 7 and resultant array will be sorted by heap with k size window
+        //max heap as smallest no we need which are closest
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Math.abs(b-x) - Math.abs(a -x)); //max-heap
+        for (int i : arr) {
+            pq.add(i);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        while (pq.size() > 0) {
+            System.out.print(pq.poll() + " ");
+        }
+        
+    }
+}
